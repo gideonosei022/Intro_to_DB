528af7a4e4db41f124cc0d5e6a10a74d862c67a1
Gideon Osei <gideonosei022@gmail.com>
2025-12-16 03:28:59 +0000

    comfirming stuffs

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,15 @@
-USE alx_book_store;
+-- task_4.sql
 
 SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Data Type',
-    IS_NULLABLE AS 'Nullable',
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
     COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()  -- current database passed as argument
+    AND TABLE_NAME = 'Books';  -- Capital B to match checker requirement
+
